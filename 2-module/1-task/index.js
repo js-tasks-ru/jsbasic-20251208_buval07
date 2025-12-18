590e1a60a80700c4604bd13a06c236c33053c270
buva <buval07@gmail.com>
2025-12-18 22:01:41 +0900

    sum-salaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,26 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
 function sumSalary(salaries) {
   // ваш код...
+  let sum = 0;
+  for (let prop in salaries) {
+    const value = salaries[prop];
+
+    if (typeof value === "number" && isFinite(value)) {
+      sum += value;
+    }
+  }
+
+  return sum;
+
 }
+
+console.log(sumSalary(salaries));
+
