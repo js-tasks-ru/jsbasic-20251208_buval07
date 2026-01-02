80cf3aa4be5a6af4b20aeb415b8e5470bf73321a
buva <buval07@gmail.com>
2026-01-02 21:35:00 +0900

    friends-create

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
   // ваш код...
+  const ul = document.createElement("UL");
+
+  friends.forEach((friend) => {
+    const li = document.createElement("LI");
+    li.textContent = `${friend.firstName} ${friend.lastName}`;
+    ul.append(li);
+  });
+
+  return ul;
 }
